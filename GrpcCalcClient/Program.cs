ff7114c197b8675cdd9afad6a552abe24cee4c16
wrong <ilak71363@gmail.com>
2022-09-06 10:24:31 +0300

    Заппилил клиент все работает

---
+++ GrpcCalcClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Net.Client;
+using GrpcCalcClient;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7204");
+
+var client = new Greeter.GreeterClient(channel);
+
+int sumA = Convert.ToInt32(Console.ReadLine());
+int sumB = Convert.ToInt32(Console.ReadLine());
+
+
+var reply = await client.AdditionalAsync(
+    new Input { A = sumA, B = sumB}
+    );
+Console.WriteLine("Сумма?==" + reply.Rezult);
